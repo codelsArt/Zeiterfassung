c07e0bc26f83b240042724fca6488fbe0b6d9cec
Lucas Mancini <lucas.mancini@espas.ch>
2021-09-09 16:41:01 +0200

    loginScript.js changed

--- target/classes/META-INF/resources/loginScript.js
+++ target/classes/META-INF/resources/loginScript.js
@@ -1,12 +1,15 @@
-$.ajax({
-    type: "POST",
-    url: 'http://localhost:8080/auth/login', // script to do the actual authentication, set session etc.
-    data: {
-        username: "email",
-        password: "password", // get element value of password here
-    },
-    success: function(data) {
-        // process result
-        console.log(data);
-    },
-});
+function submitLoginForm(){
+    console.log("hallo");
+    $.ajax({
+        type: "POST",
+        url: 'http://localhost:8080/auth/login', // script to do the actual authentication, set session etc.
+        data: {
+            username: "email",
+            password: "password", // get element value of password here
+        },
+        success: function(data) {
+            // process result
+            console.log(data);
+        },
+    });
+}
